c47a838f17502367bfc686ec927e8d932f886fc4
Jake Ross <jakerossdev@gmail.com>
2023-07-25 07:22:15 -0500

    Update bst_construction.py
    cleaner implementation, fixed some syntax errors w the variable names, shortened it up, removed redundant comments.

--- bst_construction.py
+++ bst_construction.py
@@ -1,77 +1,78 @@
-# Each instance of this class represents a node in the tree. 
-# The constructor initializes a new node with whatever given value and sets both the left and right children to None.
 class BST:
-    def __init__(self, value):
-        self.value = value # whatever value we pass in to start the tree off
-        self.left_child = None # stubbing out the placeholder for the children, initially its None, so no children yet
-        self.right_child = None     
+    def __init__(self, val):
+        self.val = val
+        self.lc = None 
+        self.rc = None     
      
-    def insert(self, value): # insertion method is called on the root node, then starts traversing
-        currrent_node = self # curr node = self gets a ref to curr instance of the class. IOW: what node are we at? (keeps track)
-        while True: # cant use for loop cuz we dont know how big tree is for traversal. gotta create infinite loop until we break
-            if value < current_node.value:
-                if current_node.left_child is None:
-                    current_node.left_child = BST(value)
+    def insert(self, val): 
+        cn = self 
+        while True: 
+            if val < cn.val:
+                if cn.lc is None:
+                    cn.lc = BST(val)
                     break
                 else:
-                    current_node = current_node.left_child
+                    cn = cn.lc
             else:
-                if current_node.right_child is None:
-                    current_node.right_child = BST(value)
+                if cn.rc is None:
+                    cn.rc = BST(val)
                     break
                 else:
-                    current_node = current_node.right_child
+                    cn = cn.rc
         return self                                                    
 
-    def contains(self, value):
-        while current_node is not None:
-            if value < current_node.value:
-                current_node = current_node.left_child
-            elif value > current_node.value:
-                current_node = current_node.right_child
+    def contains(self, val):
+        cn = self
+        while cn is not None:
+            if val < cn.val:
+                cn = cn.lc
+            elif val > cn.val:
+                cn = cn.rc
             else:
                 return True
         return False
 
-    # remove() is a 2 step process - 1) find the node - 2) actually remove it
-    # and in remove() you want to keep track of the parent node for re-assignment purposes (so the tree doesnt collapse)
-    def remove(self, value, parent_node = None): 
-        current_node = self
-        while current_node is not None: # in this while loop is the easy work of finding the node
-            if value < current_node.value:
-                parent_node = current_node
-                current_node = current_node.left_child
-            elif value > current_node.value:
-                parent_node = current_node
-                current_node = current_node.right_child
-            else: # from this else onward, we have found the node, now the hard work of removing it. (lots of edge cases)
-                # 1st case, dealing with a node that has two kids. 
-                if current_node.left_child is not None and current_node.right_child is not None:
-                    current_node.value = current_node.right_child.get_min_val() # we will build this out below. 
-                    current_node.right_child.remove(current_node.value, curr_node)
-                # 2nd case is if we dont have 2 children present (2 sub cases within that)
-                elif parent_node is None:
-                    if current_node.left_child is not None:
-                        current_node.value = current_node.left.value
-                        current_node.right_child = current_node.left_child.right_child
-                        current_node.left_child = current_node.left_child.left_child
-                    elif current_node.right_child is not None:
-                        current_node.value = current_node.right_child.value
-                        current_node.left_child = current_node.right_child.left_child
-                        current_node.right_child = current_node.right_child.right_child
+    def remove(self, val, pn = None): 
+        cn = self
+        while cn is not None:
+            if val < cn.val:
+                pn = cn
+                cn = cn.lc
+            elif val > cn.val:
+                pn = cn
+                cn = cn.rc
+            else: 
+                if cn.lc is not None and cn.rc is not None:
+                    cn.val = cn.rc.get_min_val() 
+                    cn.rc.remove(cn.val, cn)
+                elif pn is None:
+                    if cn.lc is not None:
+                        cn.val = cn.lc.val
+                        cn.rc = cn.lc.rc
+                        cn.lc = cn.lc.lc
+                    elif cn.rc is not None:
+                        cn.val = cn.rc.val
+                        cn.lc = cn.rc.lc
+                        cn.rc = cn.rc.rc
                     else:
-                        current_node.value = None
-                # so if were NOT dealing with the root node
-                # if this node has either 1 or 0 children, is the currNode a L or R child?
-                elif parent_node.left_child == current_node: # if its a L child 
-                    parent_node.left_child = currNode.left_child if current_node.left_child is not None else current_node.right_child
-                elif parent_node.right_child == current_node:
-                    parent_node.right_child = current_node.left_child if current_node.left_child is not None else current_node.right_child
+                        cn.val = None
+                elif pn.lc == cn:
+                    pn.lc = cn.lc if cn.lc is not None else cn.rc
+                elif pn.rc == cn:
+                    pn.rc = cn.lc if cn.lc is not None else cn.rc
                 break
         return self
 
     def get_min_val(self):
-        current_node = self
-        while current_node.left_child is not None:
-            current_node = current_node.left_child
-        return current_node.value
+        cn = self
+        while cn.lc is not None:
+            cn = cn.lc
+        return cn.val
+
+bst = BST(50)
+bst.insert(30)
+bst.insert(70)
+print(bst.contains(30))  # Should print True
+print(bst.contains(40))  # Should print False
+bst.remove(30)
+print(bst.contains(30))  # Should print False
